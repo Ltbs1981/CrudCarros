0fc62e7344534cc5068e18334917ed3d563e4b76
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-10 14:14:58 -0300

    classe CarroRepositorio ok

---
+++ Core/Entidades/Carro.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudCarros.Core.Entidades
+{
+    internal class Carro
+    {
+        public int Id { get; set; }
+        public string  Marca { get; set; }
+        public string  Modelo { get; set; }
+
+    }
+}
---
+++ Infra/Repositorio/CarroRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using CrudCarros.Core.Entidades;
+namespace CrudCarros.Infra.Repositorio
+{
+    internal class CarroRepositorio
+    {
+        private List<Carro> carros = new List<Carro>();
+        private static int id = 1;
+
+        public void Add(Carro carro)
+        {
+            carro.Id = id++;    
+            carros.Add(carro);  
+        }    
+        public List <Carro> GetAll()
+        {
+            return carros;
+        }
+        public void Update(Carro carro)
+        {
+            Carro banco = carros.FirstOrDefault(c => c.Id == carro.Id);
+        }
+        public void Delete(Carro carro)
+        {
+            carros.Remove(carro);
+        }
+    }
+}
