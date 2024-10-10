49542a146907b617a971d923aa7983eae4aeedd6
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-10 14:56:14 -0300

    classe servico ok

--- Servicos/CarroServico.cs
+++ Servicos/CarroServico.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using CrudCarros.Core.Entidades;
+using CrudCarros.Infra.Repositorio;
 namespace CrudCarros.Servicos
 {
     internal class CarroServico
     {
+        private CarroRepositorio _carroRepositorio;
+
+        public void Add(Carro carro)
+        {
+            _carroRepositorio.Add(carro);
+            //Console.WriteLine("Item inserido no sistema");
+            Console.WriteLine($"O carro {carro.Modelo}, foi inserido no sistema");
+        }
+        public List <Carro> GetAll()
+        {
+            return _carroRepositorio.GetAll();
+                    }
+        public void Update(int id, string novaMarca, string novoModelo)
+        {
+            var carro = _carroRepositorio.GetAll().FirstOrDefault(x => x.Id == id);
+            carro.Marca = novaMarca;
+            carro.Modelo = novoModelo;
+            _carroRepositorio.Update(carro);
+            Console.WriteLine("Item atualizado");
+        }
+        public void Delete(int id)
+        {
+            var carro = _carroRepositorio.GetAll().FirstOrDefault(c => c.Id == id);
+            _carroRepositorio.Delete(carro);
+            Console.WriteLine($"O carro: {carro.Marca}, foi apagado do sistema.");
+        }
     }
 }
